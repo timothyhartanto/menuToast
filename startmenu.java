4d63aec2651853b42c7a70cec6f0532efdfd2f99
timothyhartanto <hartantotimothy@gmail.com>
2015-11-16 22:55:52 +0700

    Create startmenu.java

---
+++ startmenu.java
@@ -0,0 +1,14 @@
+package com.example.proto.menubutton;
+
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class StartMenu extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.startmenu);
+    }
+}
